1b57d41cab8addd745f089532c0a160a761cb533
Cameron Little <cameron@camlittle.com>
2023-04-20 20:52:11 -0600

    tests

--- go.mod
+++ go.mod
@@ -21,6 +21,7 @@ require (
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
 	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
 	golang.org/x/text v0.3.2 // indirect
--- service/service_test.go
+++ service/service_test.go
@@ -7,22 +7,30 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"github.com/apexskier/strava-tile-proxy/strava"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
 type mockStravaClient struct {
-	loginCalls int
-	client     *http.Client
-}
-
-func (m *mockStravaClient) Login() error {
-	m.loginCalls++
-	return nil
+	strava.Client
+	mock.Mock
 }
 
 func (m *mockStravaClient) HttpClient() *http.Client {
-	return m.client
+	args := m.Called()
+	return args.Get(0).(*http.Client)
+}
+
+func (m *mockStravaClient) CloudFrontAuth(server string) error {
+	args := m.Called(server)
+	return args.Error(0)
+}
+
+func (m *mockStravaClient) Login() error {
+	args := m.Called()
+	return args.Error(0)
 }
 
 func TestTileService_GlobalOK(t *testing.T) {
@@ -35,20 +43,17 @@ func TestTileService_GlobalOK(t *testing.T) {
 		requestCount++
 	}))
 
-	stravaClient := mockStravaClient{
-		client: mockServer.Client(),
-	}
+	stravaClient := mockStravaClient{}
+	defer stravaClient.AssertExpectations(t)
+
+	stravaClient.On("HttpClient").Return(mockServer.Client())
+
 	s := Service{
 		stravaClient: &stravaClient,
 		logger:       log.Default(),
 		rand:         rand.New(rand.NewSource(0)),
 
-		globalHeatmapDomain:          mockServer.URL + "/server-%s",
-		athleteID:                    "12321",
-		revealPrivacyZones:           true,
-		revealOnlyMeActivities:       true,
-		revealFollowerOnlyActivities: true,
-		revealPublicActivities:       true,
+		globalHeatmapDomain: mockServer.URL + "/server-%s",
 	}
 
 	req := httptest.NewRequest("GET", "https://example.com/tiles/1/2/3", nil)
@@ -58,7 +63,6 @@ func TestTileService_GlobalOK(t *testing.T) {
 
 	require.NoError(t, err)
 	assert.Equal(t, 1, requestCount)
-	assert.Equal(t, 0, stravaClient.loginCalls)
 	assert.Equal(t, http.StatusOK, w.Code)
 }
 
@@ -72,20 +76,17 @@ func TestTileService_GlobalOK_custom_params(t *testing.T) {
 		requestCount++
 	}))
 
-	stravaClient := mockStravaClient{
-		client: mockServer.Client(),
-	}
+	stravaClient := mockStravaClient{}
+	defer stravaClient.AssertExpectations(t)
+
+	stravaClient.On("HttpClient").Return(mockServer.Client())
+
 	s := Service{
 		stravaClient: &stravaClient,
 		logger:       log.Default(),
 		rand:         rand.New(rand.NewSource(0)),
 
-		globalHeatmapDomain:          mockServer.URL + "/server-%s",
-		athleteID:                    "12321",
-		revealPrivacyZones:           true,
-		revealOnlyMeActivities:       true,
-		revealFollowerOnlyActivities: true,
-		revealPublicActivities:       true,
+		globalHeatmapDomain: mockServer.URL + "/server-%s",
 	}
 
 	req := httptest.NewRequest("GET", "https://example.com/tiles/1/2/3?color=purple&sport=winter", nil)
@@ -95,7 +96,6 @@ func TestTileService_GlobalOK_custom_params(t *testing.T) {
 
 	require.NoError(t, err)
 	assert.Equal(t, 1, requestCount)
-	assert.Equal(t, 0, stravaClient.loginCalls)
 	assert.Equal(t, http.StatusOK, w.Code)
 }
 
@@ -115,9 +115,11 @@ func TestTileService_PersonalOK(t *testing.T) {
 		requestCount++
 	}))
 
-	stravaClient := mockStravaClient{
-		client: mockServer.Client(),
-	}
+	stravaClient := mockStravaClient{}
+	defer stravaClient.AssertExpectations(t)
+
+	stravaClient.On("HttpClient").Return(mockServer.Client())
+
 	s := Service{
 		stravaClient: &stravaClient,
 		logger:       log.Default(),
@@ -137,7 +139,6 @@ func TestTileService_PersonalOK(t *testing.T) {
 
 	require.NoError(t, err)
 	assert.Equal(t, 1, requestCount)
-	assert.Equal(t, 0, stravaClient.loginCalls)
 	assert.Equal(t, http.StatusOK, w.Code)
 }
 
@@ -151,9 +152,11 @@ func TestTileService_PersonalOK_custom_params(t *testing.T) {
 		requestCount++
 	}))
 
-	stravaClient := mockStravaClient{
-		client: mockServer.Client(),
-	}
+	stravaClient := mockStravaClient{}
+	defer stravaClient.AssertExpectations(t)
+
+	stravaClient.On("HttpClient").Return(mockServer.Client())
+
 	s := Service{
 		stravaClient: &stravaClient,
 		logger:       log.Default(),
@@ -173,11 +176,10 @@ func TestTileService_PersonalOK_custom_params(t *testing.T) {
 
 	require.NoError(t, err)
 	assert.Equal(t, 1, requestCount)
-	assert.Equal(t, 0, stravaClient.loginCalls)
 	assert.Equal(t, http.StatusOK, w.Code)
 }
 
-func TestTileService_401(t *testing.T) {
+func TestTileService_Personal401(t *testing.T) {
 	requestCount := 0
 	mockServer := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
 		assert.Equal(t, "/tiles/12321/red/1/2/3@2x.png", r.URL.Path)
@@ -189,9 +191,12 @@ func TestTileService_401(t *testing.T) {
 		requestCount++
 	}))
 
-	stravaClient := mockStravaClient{
-		client: mockServer.Client(),
-	}
+	stravaClient := mockStravaClient{}
+	defer stravaClient.AssertExpectations(t)
+
+	stravaClient.On("HttpClient").Return(mockServer.Client())
+	stravaClient.On("Login").Return(nil)
+
 	s := Service{
 		stravaClient: &stravaClient,
 		logger:       log.Default(),
@@ -211,7 +216,6 @@ func TestTileService_401(t *testing.T) {
 
 	require.NoError(t, err)
 	assert.Equal(t, 2, requestCount)
-	assert.Equal(t, 1, stravaClient.loginCalls)
 	assert.Equal(t, http.StatusOK, w.Code)
 }
 
@@ -260,4 +264,40 @@ func TestTileService_400(t *testing.T) {
 
 	require.NoError(t, err)
 	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
+
+func TestTileService_Global403(t *testing.T) {
+	requestCount := 0
+	mockServer := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "/server-a/tiles-auth/all/red/1/2/3@2x.png", r.URL.Path)
+		if requestCount == 0 {
+			rw.WriteHeader(http.StatusUnauthorized)
+		} else {
+			rw.WriteHeader(http.StatusOK)
+		}
+		requestCount++
+	}))
+
+	stravaClient := mockStravaClient{}
+	defer stravaClient.AssertExpectations(t)
+
+	stravaClient.On("HttpClient").Return(mockServer.Client())
+	stravaClient.On("CloudFrontAuth", "a").Return(nil)
+
+	s := Service{
+		stravaClient: &stravaClient,
+		logger:       log.Default(),
+		rand:         rand.New(rand.NewSource(0)),
+
+		globalHeatmapDomain: mockServer.URL + "/server-%s",
+	}
+
+	req := httptest.NewRequest("GET", "https://example.com/tiles/1/2/3", nil)
+	w := httptest.NewRecorder()
+
+	err := s.ServeGlobalTile(w, req)
+
+	require.NoError(t, err)
+	assert.Equal(t, 2, requestCount)
+	assert.Equal(t, http.StatusOK, w.Code)
 }
